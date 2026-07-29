ac43f505fbb95fd10bfb2a387d0cedf70b7781cb
Surana-Piyush <epicpiyush@gmail.com>
2026-07-30 00:06:45 +0530

    Register endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy import select
 
-from database import get_db
-from models import User
+from database import get_db, User
 from schema import UserCreate
 from auth import hash_password
+
 app = FastAPI()
 
-@app.get("/register")
-def home():
-    return {"message": "Movie Recommendation API"}
+@app.post("/register")
+def register(user: UserCreate, db: Session = Depends(get_db)):
+
+    existing_user = db.execute(
+        select(User).where(User.email == user.email)
+    ).scalar_one_or_none()
+
+    if existing_user:
+        raise HTTPException(
+            status_code=400,
+            detail="Email already registered"
+        )
+
+    return {
+        "Name": user.username,
+        "Email": user.email
+    }
