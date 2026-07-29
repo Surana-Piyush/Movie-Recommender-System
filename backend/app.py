7a3a1bfe640f393c04f1f5dd628fac73d948f8e7
Surana-Piyush <epicpiyush@gmail.com>
2026-07-30 00:09:08 +0530

    Register Endpoint

--- backend/app.py
+++ backend/app.py
@@ -8,9 +8,11 @@ from auth import hash_password
 
 app = FastAPI()
 
+
 @app.post("/register")
 def register(user: UserCreate, db: Session = Depends(get_db)):
 
+    # Check if email already exists
     existing_user = db.execute(
         select(User).where(User.email == user.email)
     ).scalar_one_or_none()
@@ -21,7 +23,23 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
             detail="Email already registered"
         )
 
+    hashed_password = hash_password(user.password)
+
+    new_user = User(
+        username=user.username,
+        email=user.email,
+        password_hash=hashed_password
+    )
+
+    db.add(new_user)
+    db.commit()
+
+    db.refresh(new_user)
+
+    # Return response
     return {
-        "Name": user.username,
-        "Email": user.email
+        "message": "User registered successfully",
+        "user_id": new_user.user_id,
+        "username": new_user.username,
+        "email": new_user.email
     }
