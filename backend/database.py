37821cff4c0b0487a1687beb87c5ea22e9d941cd
Surana-Piyush <epicpiyush@gmail.com>
2026-09-01 18:42:04 +0530

    feat: initialize SQLAlchemy database schema and remove Render configuration file

--- backend/database.py
+++ backend/database.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from sqlalchemy import String, Integer, Column, create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 BASE_DIR = Path(__file__).resolve().parent
 DB_URL = f"sqlite:///{BASE_DIR / 'movies.db'}"
@@ -13,20 +15,22 @@ SessionLocal = sessionmaker(
 
 
 class User(Base):
-    __tablename__="users"
+    __tablename__ = "users"
 
-    user_id=Column(Integer,primary_key=True)
+    user_id = Column(Integer, primary_key=True)
     username = Column(String, unique=True, nullable=False)
     email = Column(String, unique=True, nullable=False)
     password_hash = Column(String, nullable=False)
+
 
 class Rating(Base):
     __tablename__ = "ratings"
 
     rating_id = Column(Integer, primary_key=True)
-    user_id=Column(Integer)
+    user_id = Column(Integer)
     movie_id = Column(Integer)
     rating = Column(Integer)
+
 
 class Watchlist(Base):
     __tablename__ = "watchlist"
@@ -35,7 +39,9 @@ class Watchlist(Base):
     user_id = Column(Integer, nullable=False)
     movie_id = Column(Integer, nullable=False)
 
+
 Base.metadata.create_all(bind=engine)
+
 
 def get_db():
     db = SessionLocal()
@@ -43,4 +49,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
